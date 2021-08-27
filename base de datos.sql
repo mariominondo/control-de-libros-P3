734284d485564b19f833fe05a16b7b69d49e1ef7
Mario Minondo <mariusminondus@yahoo.com>
2021-08-27 17:25:54 -0600

    Database modification. Created Libros window and DB Insertion.

---
+++ base de datos.sql
@@ -0,0 +1,144 @@
+-- MariaDB dump 10.19  Distrib 10.4.20-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: control_de_libros
+-- ------------------------------------------------------
+-- Server version	10.4.20-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Juan','Guatemala','+502 55555555','juan@gmail.com'),(2,'Pedro','Peten','+502 55556666','pedro@email.com'),(3,'Marta','Huehuetenango','+502 66665555','marta@mail.com'),(5,'Maria','Huehuetenango','+502 66665555','marta@mail.com');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ejemplares`
+--
+
+DROP TABLE IF EXISTS `ejemplares`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ejemplares` (
+  `numero` int(11) NOT NULL,
+  `referencia_libro` int(11) NOT NULL,
+  PRIMARY KEY (`numero`,`referencia_libro`),
+  KEY `referencia_libro` (`referencia_libro`),
+  CONSTRAINT `ejemplares_ibfk_1` FOREIGN KEY (`referencia_libro`) REFERENCES `libros` (`codigo_libro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ejemplares`
+--
+
+LOCK TABLES `ejemplares` WRITE;
+/*!40000 ALTER TABLE `ejemplares` DISABLE KEYS */;
+INSERT INTO `ejemplares` VALUES (1,1);
+/*!40000 ALTER TABLE `ejemplares` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libros`
+--
+
+DROP TABLE IF EXISTS `libros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `libros` (
+  `codigo_libro` int(11) NOT NULL,
+  `titulo` varchar(45) DEFAULT NULL,
+  `genero_literario` varchar(45) DEFAULT NULL,
+  `editorial` varchar(45) DEFAULT NULL,
+  `autor` varchar(45) DEFAULT NULL,
+  `anio_edicion` varchar(45) DEFAULT NULL,
+  `numero_edicion` varchar(45) DEFAULT NULL,
+  `pais_origen` varchar(45) NOT NULL,
+  `numero_paginas` varchar(45) DEFAULT NULL,
+  `cantidad_ejemplares` varchar(45) DEFAULT NULL,
+  `precio` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`codigo_libro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libros`
+--
+
+LOCK TABLES `libros` WRITE;
+/*!40000 ALTER TABLE `libros` DISABLE KEYS */;
+INSERT INTO `libros` VALUES (1,'El PRincipito','Fantasia','Richmond','David Arreaga','2000','tercera','Guatemala','100','1000','Q150');
+/*!40000 ALTER TABLE `libros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ventas`
+--
+
+DROP TABLE IF EXISTS `ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ventas` (
+  `numero_pedido` int(11) NOT NULL,
+  `fecha_pedido` varchar(45) DEFAULT NULL,
+  `referencia_libro` int(11) NOT NULL,
+  `referencia_cliente` int(11) NOT NULL,
+  `cantidad_compra` int(11) DEFAULT NULL,
+  `monto_total` decimal(2,0) DEFAULT NULL,
+  PRIMARY KEY (`numero_pedido`),
+  KEY `referencia_libro` (`referencia_libro`),
+  KEY `referencia_cliente` (`referencia_cliente`),
+  CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`referencia_libro`) REFERENCES `libros` (`codigo_libro`),
+  CONSTRAINT `ventas_ibfk_2` FOREIGN KEY (`referencia_cliente`) REFERENCES `clientes` (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ventas`
+--
+
+LOCK TABLES `ventas` WRITE;
+/*!40000 ALTER TABLE `ventas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ventas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-27 17:17:34
