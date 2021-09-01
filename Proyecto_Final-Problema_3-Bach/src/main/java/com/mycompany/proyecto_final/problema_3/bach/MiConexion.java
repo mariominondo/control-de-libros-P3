5c417bbda36d1b0695f345aeace3e7b546719b1b
Mario Minondo <mariusminondus@yahoo.com>
2021-09-01 12:53:28 -0600

    modificacion de la clase conexion

---
+++ Proyecto_Final-Problema_3-Bach/src/main/java/com/mycompany/proyecto_final/problema_3/bach/MiConexion.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.proyecto_final.problema_3.bach;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MiConexion {
+    
+    private final String bd = "control_de_libros";
+    private final String user = "root";
+    private final String password = "";    
+    private final String url = "jdbc:mysql://localhost/" + bd;
+    
+    private Connection con = null;
+    
+    public Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(this.url, this.user, this.password);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        return con;
+    }
+}
